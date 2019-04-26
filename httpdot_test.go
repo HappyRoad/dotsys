0af38fa1740e3e0ecac4933eafdfc82baeafd590
邓小路 <549473250@qq.com>
2019-04-26 13:29:46 +0800

    remove server

--- httpdot_test.go
+++ httpdot_test.go
@@ -1,13 +1,16 @@
 package dotsys
 
 import (
+	"io/ioutil"
+	"log"
+	"net/http"
 	"sync"
 	"testing"
 	"time"
 )
 
 func TestHttpDot(t *testing.T) {
-	/*go func() {
+	go func() {
 		http.HandleFunc("/heartbeat", func(w http.ResponseWriter, r *http.Request) {
 			result, _ := ioutil.ReadAll(r.Body)
 			r.Body.Close()
@@ -16,7 +19,6 @@ func TestHttpDot(t *testing.T) {
 
 		http.ListenAndServe(":7777", nil)
 	}()
-	*/
 	time.Sleep(time.Second * 3)
 
 	dot, err := NewHttpDot("test")
